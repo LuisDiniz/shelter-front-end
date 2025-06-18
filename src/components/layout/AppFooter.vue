2da11117f59ddc3a6cbf9fdc99cf7d0aa2840aee
Luis Diniz <luis.diniz.carvalho@gmail.com>
2025-06-18 22:46:22 +0100

    Fix footer elements alignment

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -7,9 +7,9 @@ const currentYear = ref(new Date().getFullYear())
 <template>
   <footer class="bg-secondary-800 text-white py-10 mt-12">
     <div class="container mx-auto px-4">
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-16">
         <!-- Contact Information -->
-        <div>
+        <div class="flex flex-col">
           <h3 class="text-xl font-semibold mb-4">Entre em contacto</h3>
           <div class="flex flex-col space-y-2">
             <p class="flex items-center">
@@ -36,54 +36,58 @@ const currentYear = ref(new Date().getFullYear())
         </div>
 
         <!-- Quick Links -->
-        <div>
-          <h3 class="text-xl font-semibold mb-4">Links rápidos</h3>
-          <ul class="space-y-2">
-            <li>
-              <router-link to="/animais" class="text-white hover:text-primary-300 transition-colors">
-                Nossos Animais
-              </router-link>
-            </li>
-            <li>
-              <router-link to="/como-ajudar" class="text-white hover:text-primary-300 transition-colors">
-                Como Ajudar
-              </router-link>
-            </li>
-            <li>
-              <router-link to="/historia" class="text-white hover:text-primary-300 transition-colors">
-                História
-              </router-link>
-            </li>
-            <li>
-              <router-link to="/noticias" class="text-white hover:text-primary-300 transition-colors">
-                Notícias
-              </router-link>
-            </li>
-          </ul>
+        <div class="flex flex-col md:justify-center md:items-center">
+          <div class="md:text-left">
+            <h3 class="text-xl font-semibold mb-4">Links rápidos</h3>
+            <ul class="space-y-2">
+              <li>
+                <router-link to="/animais" class="text-white hover:text-primary-300 transition-colors">
+                  Nossos Animais
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/como-ajudar" class="text-white hover:text-primary-300 transition-colors">
+                  Como Ajudar
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/historia" class="text-white hover:text-primary-300 transition-colors">
+                  História
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/noticias" class="text-white hover:text-primary-300 transition-colors">
+                  Notícias
+                </router-link>
+              </li>
+            </ul>
+          </div>
         </div>
 
         <!-- Social Media -->
-        <div>
-          <h3 class="text-xl font-semibold mb-4">Siga-nos</h3>
-          <div class="flex space-x-4">
-            <a href="https://m.facebook.com/100064554048217/?locale=pt_PT" class="text-white hover:text-primary-300 transition-colors">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.325v-21.35c0-.732-.593-1.325-1.325-1.325z"/>
-                <title>Facebook</title>
-              </svg>
-            </a>
-            <a href="https://www.instagram.com/voluntarios_seixal?igsh=am05enl0YnZsN2pr" class="text-white hover:text-primary-300 transition-colors">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
-                <title>Instagram</title>
-              </svg>
-            </a>
-            <a href="https://www.tiktok.com/@voluntarios_seixal?_t=ZG-8xG6iFIh6bh&_r=1" class="text-white hover:text-primary-300 transition-colors">          
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-.88-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z"/>
-                <title>TikTok</title>
-              </svg>
-            </a>
+        <div class="flex flex-col md:items-end">
+          <div class="flex flex-col">
+            <h3 class="text-xl font-semibold mb-4 md:text-left">Siga-nos</h3>
+            <div class="flex space-x-4 md:justify-end">
+              <a href="https://m.facebook.com/100064554048217/?locale=pt_PT" class="text-white hover:text-primary-300 transition-colors">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                  <path d="M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.325v-21.35c0-.732-.593-1.325-1.325-1.325z"/>
+                  <title>Facebook</title>
+                </svg>
+              </a>
+              <a href="https://www.instagram.com/voluntarios_seixal?igsh=am05enl0YnZsN2pr" class="text-white hover:text-primary-300 transition-colors">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                  <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
+                  <title>Instagram</title>
+                </svg>
+              </a>
+              <a href="https://www.tiktok.com/@voluntarios_seixal?_t=ZG-8xG6iFIh6bh&_r=1" class="text-white hover:text-primary-300 transition-colors">          
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                  <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-.88-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z"/>
+                  <title>TikTok</title>
+                </svg>
+              </a>
+            </div>
           </div>
         </div>
       </div>
